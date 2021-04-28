f97a6968a807dfadba48d63d9f92f28369e986be
al <alex.pobery@gmail.com>
2021-04-27 21:54:06 -0700

    added icons

--- app/src/main/java/com/example/covid/datasource/RemoteDataSource.kt
+++ app/src/main/java/com/example/covid/datasource/RemoteDataSource.kt
@@ -8,6 +8,7 @@ import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import java.lang.Exception
 
